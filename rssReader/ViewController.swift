a57f8b0906404f46b1c2692c81f5f316983f6c1b
v-liberov <v-liberov@yandex-team.ru>
2019-02-08 16:44:04 +0700

    add simple button, label and connect with actions

--- rssReader/ViewController.swift
+++ rssReader/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("view did load!")
     }
-
-
+    
+    @IBAction func onTapButton(_ sender: UIButton) {
+        label.text = "LOL!"
+    }
 }
 
