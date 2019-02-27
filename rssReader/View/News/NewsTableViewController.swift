c66eeaf8599a4bf7bd843f8a17efa9135eb08da2
v-liberov <v-liberov@yandex-team.ru>
2019-02-27 10:02:00 +0700

    add select filter via alert

--- rssReader/View/News/NewsTableViewController.swift
+++ rssReader/View/News/NewsTableViewController.swift
@@ -9,8 +9,53 @@
 import UIKit
 import RxSwift
 
-class NewsTableViewController: UITableViewController {
+class NewsTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let filters = ["Все новости", "Избранные новости"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.filters.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.filters[row]
+    }
+    
     var notes: Array<RssNews> = [];
+    
+    @IBOutlet weak var newsFilter: UIBarButtonItem!
+    
+    @IBAction func onTapFilter(_ sender: UIBarButtonItem) {
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: 250,height: 300)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 250, height: 300))
+        pickerView.delegate = self;
+        pickerView.dataSource = self;
+        vc.view.addSubview(pickerView)
+        let editRadiusAlert = UIAlertController(
+            title: "Сменить фильтр",
+            message: "",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        editRadiusAlert.setValue(vc, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(
+            title: "Выбрать",
+            style: .default,
+            handler: {(action: UIAlertAction) in
+                self.newsFilter.title = self.filters[pickerView.selectedRow(inComponent: 0)]
+            })
+        )
+        editRadiusAlert.addAction(UIAlertAction(
+            title: "Отмена",
+            style: .cancel,
+            handler: nil)
+        )
+        self.present(editRadiusAlert, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,22 +67,14 @@ class NewsTableViewController: UITableViewController {
                 self.tableView.reloadData();
             })
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        newsFilter.title = self.filters[0];
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return notes.count
     }
 
@@ -45,7 +82,6 @@ class NewsTableViewController: UITableViewController {
         func nestedTap() {
             self.performSegue(withIdentifier: "showFullNewsSeque", sender: link)
         }
-        
         return nestedTap
     }
     
@@ -73,52 +109,10 @@ class NewsTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nextViewController = segue.destination as? FullNewsViewController {
             let message: String = sender as! String;
             nextViewController.newsLink = message;
         }
     }
- 
-
 }
